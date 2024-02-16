7af992cbe719dd5def956cf9425ffd673fd44798
Heero <techheero@gmail.com>
2024-02-15 22:03:28 -0600

    finished all 12 task Iam now the best

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,8 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    //we want the titles of the books in array
+    //how do we get values out of arraays
+    return books.map(book=> book.title);
+
 
 };
 
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (DOB) {
+  //figure out who the oldest is
+  let oldestPerson = DOB.reduce((oldestOne, youngestOne) => {
+    let oldestAge = getAge(oldestOne);
+    let currentAge = getAge(youngestOne);
+    //ternary uses the '?' to then get value out of the variable via
+    //object DOB
+    return oldestAge > currentAge ? oldestOne : youngestOne;
+  });
+  return oldestPerson;
+};
+//this is a function
+//finding age of unkown, use todays date
+//finding age of known
+//this also is a fuction
+const getAge = function (person) {
+  if (!person.yearOfDeath) {
+    //how to get the date
+    let currentYear = new Date().getFullYear();
+    return currentYear - person.yearOfBirth;
+  } else {
+    return person.yearOfDeath - person.yearOfBirth;
+  }
 };
 
 // Do not edit below this line
